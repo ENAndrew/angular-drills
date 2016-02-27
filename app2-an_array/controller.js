aa702ef310dee051409a1160190346939ec0eeb6
Erica Andrew <erica.nicole.andrew@gmail.com>
2016-02-27 10:22:37 -0700

    Completed App 2

---
+++ app2-an_array/controller.js
@@ -0,0 +1,11 @@
+
+var app = angular.module('arrayApp');
+
+app.controller('arrayController', function($scope, arrayService){
+   
+   $scope.friendList = arrayService.myData;  //this is getting data correctly
+   
+  
+    
+    
+});
